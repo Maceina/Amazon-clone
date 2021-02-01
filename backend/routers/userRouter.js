ab459878dd6ed382e9af7c64bfd5788d953191b4
Maceina <maceinaaudrius@gmail.com>
2021-02-01 12:29:15 +0200

    conected mongo db

---
+++ backend/routers/userRouter.js
@@ -0,0 +1,18 @@
+import express from "express";
+import User from "../models/userModel.js";
+import expressAsyncHandler from 'express-async-handler';
+import data from "../data.js";
+
+const userRouter = express.Router();
+
+userRouter.get(
+  "/seed",
+  expressAsyncHandler(async (req, res) => {
+      //Atention!!! removes all users
+      await User.remove({});
+    const createdUsers = await User.insertMany(data.users);
+    res.send({ createdUsers });
+  })
+);
+
+export default userRouter;
