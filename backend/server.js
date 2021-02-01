d6b2023d2f5043a7042f147ef6bf0e3caa36c6a0
Maceina <maceinaaudrius@gmail.com>
2021-02-01 12:55:53 +0200

    fetching products from database

--- backend/server.js
+++ backend/server.js
@@ -1,38 +1,23 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import data from './data.js';
-import userRouter from './routers/userRouter.js';
+import express from "express";
+import mongoose from "mongoose";
+import productRouter from "./routers/productRouter.js";
+import userRouter from "./routers/userRouter.js";
 
 const app = express();
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazon',{
+mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/amazon", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
 });
 
-app.get('/api/products/:id', (req, res) => {
-  const product = data.products.find((x) => x._id === req.params.id);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: 'Product Not Found' });
-  }
+app.use("/api/users/", userRouter);
+app.use("/api/products", productRouter);
+app.get("/", (req, res) => {
+  res.send("Server is ready");
 });
-
-app.get('/api/products', (req, res) => {
-  res.send(data.products);
-});
-
-app.use('/api/users/', userRouter);
-
-app.get('/', (req, res) => {
-  res.send('Server is ready');
-});
-
 app.use((err, req, res, next) => {
-  res.status(500).send({message: err.message});
+  res.status(500).send({ message: err.message });
 });
-
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`);
