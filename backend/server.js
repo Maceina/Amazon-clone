803e476c31ee0d383468013aa9c0d8aeca9a44a1
Maceina <maceinaaudrius@gmail.com>
2021-02-04 12:49:52 +0200

    changing project structure

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 //moved from localhost to cloud
-mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/amazon", {
+mongoose.connect(process.env.MONGODB_URL || "mongodb+srv://admin:7Vpc4bRTcaspmirK@cluster0.zrlyb.mongodb.net/amazon?retryWrites=true&w=majority", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
